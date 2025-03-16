238788416ba61f99b370757cccd35d213f9e6006
RAHUL <rahulsd380@gmail.com>
2025-03-16 11:15:42 +0600

    Added assets file

---
+++ public/assets/index.ts
@@ -0,0 +1,21 @@
+import dummyLogo from "./images/dummy-logo.png";
+import gradientBg from "./images/gradient-bg.svg";
+
+import ellipse from "./icons/ellipse.svg";
+import instagram from "./icons/instagram.svg";
+import search from "./icons/search.svg";
+import rightArrow from "./icons/right-arrow.svg";
+import downArrow from "./icons/down-arrow.svg";
+
+export const IMAGES = {
+    dummyLogo,
+    gradientBg,
+};
+
+export const ICONS = {
+    ellipse,
+    instagram,
+    search,
+    rightArrow,
+    downArrow,
+};
