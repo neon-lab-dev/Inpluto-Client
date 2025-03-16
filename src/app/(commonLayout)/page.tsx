f914ac2fcde196ad19ac47d392ce9bcb01a38693
RAHUL <rahulsd380@gmail.com>
2025-03-16 11:29:43 +0600

    Added why inpluto section

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,10 +1,12 @@
 import FeaturesTab from "@/components/HomePage/FeaturesTab/FeaturesTab";
+import WhyInpluto from "@/components/Shared/WhyInpluto/WhyInpluto";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="">
       <FeaturesTab/>
+      <WhyInpluto/>
     </div>
   );
 }
---
+++ src/components/Shared/WhyInpluto/WhyInpluto.tsx
@@ -0,0 +1,23 @@
+import Container from "@/components/Reusable/Container/Container";
+import { IMAGES } from "../../../../public/assets";
+import Image from "next/image";
+
+const WhyInpluto = () => {
+    return (
+        <div className="bg-white pt-8">
+            <Container>
+                <h1 className="text-[#364153] font-Satoshi-500 text-xl leading-6 text-center">Why you must choose Inpluto</h1>
+                <div className="flex items-center justify-between gap-[96px] mt-[60px] pb-7">
+                    <Image src={IMAGES.dummyLogo} alt="" />
+                    <Image src={IMAGES.dummyLogo} alt="" />
+                    <Image src={IMAGES.dummyLogo} alt="" />
+                    <Image src={IMAGES.dummyLogo} alt="" />
+                    <Image src={IMAGES.dummyLogo} alt="" />
+                    <Image src={IMAGES.dummyLogo} alt="" />
+                </div>
+            </Container>
+        </div>
+    );
+};
+
+export default WhyInpluto;
