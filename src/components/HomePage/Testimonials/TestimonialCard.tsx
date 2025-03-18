42281021d79fec699e7882c706a0810d87703a95
RAHUL <rahulsd380@gmail.com>
2025-03-18 14:47:30 +0600

    completed testimonial section with marquee

--- src/components/HomePage/Testimonials/TestimonialCard.tsx
+++ src/components/HomePage/Testimonials/TestimonialCard.tsx
@@ -1,17 +1,20 @@
 
-const TestimonialCard = () => {
+type TTestimonialCard = {
+    name: string;
+    brandName: string;
+    review: string;
+}
+const TestimonialCard: React.FC<TTestimonialCard> = ({ name, brandName, review }) => {
     return (
-        <div className="bg-neutral-45 rounded-[20px] p-6 font-Satoshi max-w-[400px] h-[326px] w-full flex flex-col justify-between gap-6">
-            <div className="h-[209px]">
-                <p className="text-gray-20 text-lg leading-[26px]">
-                    Lorem ipsum dolor sit amet consectetur. Dignissim massa porta ornare duis sed tellus sapien sagittis. Feugiat donec a montes mi elit vel ultrices sit vestibulum. Molestie augue in orci ridiculus aenean vitae volutpat in et.
+        <div className="bg-neutral-45 rounded-[20px] p-6 font-Satoshi max-w-[400px] h-[326px] w-full flex flex-col justify-between gap-6 mr-6">
+                <p className="text-gray-20 text-lg leading-[26px] h-[209px] overflow-y-auto custom-scrollbar">
+                    {review}
                 </p>
-            </div>
 
             <div>
-                <h1 className="text-white text-lg font-bold">Salmaan Ahmed</h1>
+                <h1 className="text-white text-lg font-bold">{name}</h1>
                 <p className="text-neutral-60">
-                    Brand Name
+                    {brandName}
                 </p>
             </div>
         </div>
--- src/components/HomePage/Testimonials/Testimonials.tsx
+++ src/components/HomePage/Testimonials/Testimonials.tsx
@@ -1,11 +1,43 @@
 import Heading from "@/components/Reusable/Heading/Heading";
 import TestimonialCard from "./TestimonialCard";
+import Marquee from "react-fast-marquee";
 
 const Testimonials = () => {
+    const testimonials = [
+        {
+            name: "Rahul Sutradhar",
+            brandName: "MITR Consultancy",
+            review: "Lorem ipsum dolor sit amet consectetur. Dignissim massa porta ornare duis sed tellus sapien sagittis. Feugiat donec a montes mi elit vel ultrices sit vestibulum. Molestie augue in orci ridiculus aenean vitae volutpat in et. Lorem ipsum dolor sit amet consectetur. Dignissim massa porta ornare duis sed tellus sapien sagittis. Feugiat donec a montes mi elit vel ultrices sit vestibulum. Molestie augue in orci ridiculus aenean vitae volutpat in et."
+        },
+        {
+            name: "Rahul Sutradhar",
+            brandName: "MITR Consultancy",
+            review: "Lorem ipsum dolor sit amet consectetur. Dignissim massa porta ornare duis sed tellus sapien sagittis. Feugiat donec a montes mi elit vel ultrices sit vestibulum. Molestie augue in orci ridiculus aenean vitae volutpat in et."
+        },
+        {
+            name: "Rahul Sutradhar",
+            brandName: "MITR Consultancy",
+            review: "Lorem ipsum dolor sit amet consectetur. Dignissim massa porta ornare duis sed tellus sapien sagittis. Feugiat donec a montes mi elit vel ultrices sit vestibulum. Molestie augue in orci ridiculus aenean vitae volutpat in et."
+        },
+        {
+            name: "Rahul Sutradhar",
+            brandName: "MITR Consultancy",
+            review: "Lorem ipsum dolor sit amet consectetur. Dignissim massa porta ornare duis sed tellus sapien sagittis. Feugiat donec a montes mi elit vel ultrices sit vestibulum. Molestie augue in orci ridiculus aenean vitae volutpat in et."
+        },
+        {
+            name: "Rahul Sutradhar",
+            brandName: "MITR Consultancy",
+            review: "Lorem ipsum dolor sit amet consectetur. Dignissim massa porta ornare duis sed tellus sapien sagittis. Feugiat donec a montes mi elit vel ultrices sit vestibulum. Molestie augue in orci ridiculus aenean vitae volutpat in et."
+        },
+    ];
     return (
         <div className=" ">
             <Heading heading="Our Testimonials" description="See how brands and creators are transforming their campaigns with INPLUTO." />
-            <TestimonialCard/>
+            <Marquee speed={30} className="flex items-center mt-[64px]" direction="left" pauseOnHover>
+                {testimonials.map((testimonial) => (
+                    <TestimonialCard key={testimonial.review} {...testimonial} />
+                ))}
+            </Marquee>
         </div>
     );
 };
